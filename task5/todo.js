d02f54917be6020d76c511a98891685121c6eeea
HubaBuba <ilyer76@yandex.ru>
2023-12-20 12:46:51 +0300

    task5

---
+++ task5/todo.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('todoInput')
+function addTodo() {
+const ul = document.getElementById('todoList');
+const li = document.createElement('li');
+
+li.textContent = input.value;
+input.value = ""
+ul.appendChild(li);
+}
+
+addEventListener('keydown', function(event){
+	if (event.key === "Enter") addTodo();
+})
